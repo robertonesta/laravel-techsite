5ae5c497b1a386b7cf1ab602ea079aaab42f318e
RobertoNesta <robertonesta1993@gmail.com>
2023-09-28 17:55:29 +0200

    adding some margin bottom to the card div

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">{{$product->name}}</h2>
-    <div class="card">
+    <div class="card mb-5">
         <div class="card-body text-center">
             <div id="image_switch" class="d-flex justify-content-center">
-                <img class="image w-50 card-img-top my-3 d-block" src="{{$product->image}}" alt="">
+                <img class="image w-25 card-img-top my-3 d-block" src="{{$product->image}}" alt="">
             </div>
         </div>
         <div class="card-footer bg-transparent py-3 text-center">
